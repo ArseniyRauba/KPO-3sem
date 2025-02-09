5568959d80ca2ff0c130171215c605ebefafb346
Arseniy Rauba <155465667+ArseniyRauba@users.noreply.github.com>
2025-02-09 11:49:31 +0300

    Add files via upload

---
+++ asm1/SE_asm01/SE_asm01/1.asm
@@ -0,0 +1,30 @@
+.586
+.MODEL FLAT, STDCALL
+includelib kerne132.lib
+
+ExitProcess PROTO : DWORD
+MessageBoxA PROTO : DWORD, : DWORD, : DWORD, : DWORD
+
+.STACK 4096
+
+.DATA
+MB_OK EQU 0
+STR1 DB "Моя первая программа", 0
+STR2 DB "Привет всем!", 0
+HW DD ?
+
+.CODE
+
+main PROC
+START : 
+push MB_OK
+push OFFSET STR1
+push OFFSET STR2
+push HW
+call MessageBoxA
+
+	push - 1
+	call ExitProcess
+main ENDP
+
+end main
