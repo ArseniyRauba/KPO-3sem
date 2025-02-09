58281eef78d6b8e465f4129e2f30302a00ce626e
Arseniy Rauba <155465667+ArseniyRauba@users.noreply.github.com>
2025-02-09 11:48:23 +0300

    Add files via upload

---
+++ 19/SE_asm01с/SE_asm01с.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#pragma comment(lib, "..\\Debug\\SE_asm01a.lib")
+extern "C"
+{
+    int __stdcall getMin(int*, int);
+    int __stdcall getMax(int*, int);
+}
+
+int main()
+{
+    int arr[10] = { 2, 6, 8, 87, 3, 1, 12, 9, 11, 15 };
+
+    std::cout << "getmax + getmin = " << getMin(arr, 10) + getMax(arr, 10) << std::endl;
+}
